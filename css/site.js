1846426e75f921f2d01f62351c151e5bd5f6bcf1
patrick-tse <tpatpat@gmail.com>
2020-08-17 11:37:00 -0700

    Init commit

---
+++ css/site.js
@@ -0,0 +1,27 @@
+﻿window.showModal = () => {
+    $('#exampleModal').modal('show');
+    $('#exampleModal').on('hide.bs.modal', onModalHide);
+};
+
+window.hideModal = () => {
+    $('#exampleModal').modal('hide');
+};
+
+window.onModalHide = () => {
+    DotNet.invokeMethodAsync('PartnerProducts.Client', 'InvokeShowPreviewChanged');
+};
+
+window.clipboardCopy = {
+    copyText: function (text) {
+        navigator.clipboard.writeText(text).then(function () {
+            /*alert("Copied to clipboard!");*/
+        })
+            .catch(function (error) {
+                alert(error);
+            });
+    }
+};
+
+window.scrollUp = () => {
+    $('#topofpage')[0].scrollIntoView({ behavior: 'smooth' });
+};
